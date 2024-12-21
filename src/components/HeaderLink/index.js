9e8b30af85976f846deff8ef5fd42d3992071f8f
Kai Ximenes <kaiximenes@outlook.com>
2024-12-21 10:07:39 -0300

    Header completo

--- src/components/HeaderLink/index.js
+++ src/components/HeaderLink/index.js
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './HeaderLink.module.css';
 
 function HeaderLink({ url, children }) {
+
+    const location = useLocation();
+
+    const isActive = location.pathname === url;
+
     return (
-        <Link to={url} className={styles.link}>
+        <Link 
+            to={url} 
+            className={`${styles.link} ${isActive ? styles.active : ''}`}>
             {children}
         </Link>
     )
