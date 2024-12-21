b99f017ec4c4a7a856fa1127a95fcd3f36766837
Kai Ximenes <kaiximenes@outlook.com>
2024-12-21 12:22:05 -0300

    Rotas ajustadas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import NewVideo from './pages/NewVideo';
-import AppRoutes from './routes';
+import AppRoutes from './routes'; // Importando o arquivo de rotas
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-        <Home />
+      <AppRoutes />  {/* As rotas serão inseridas aqui */}
       <Footer />
     </BrowserRouter>
-    
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import NewVideo from "./pages/NewVideo";
 
 function AppRoutes() {
     return (
-        <BrowserRouter>
-            <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/newvideo" element={<NewVideo />}></Route>
-                    <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/newvideo" element={<NewVideo />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default AppRoutes;
